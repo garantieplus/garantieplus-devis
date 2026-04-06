16de2e0a993d74a24fb581542ed0051237ea5e8a
Benjamin Attal <benjaminattal@MacBook-Pro-de-Benjamin-3.local>
2026-04-06 11:58:56 +0200

    Redirection partenaire.garantieplus.fr -> /inscription

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [{ type: 'host', value: 'partenaire.garantieplus.fr' }],
+        destination: 'https://devis.garantieplus.fr/inscription',
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
